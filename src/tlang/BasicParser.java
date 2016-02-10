1e276d5b619db89d1d27ec4c948a48d7a4f07057
Jerry <chenjian@grinnell.edu>
2016-02-10 10:52:15 -0600

    parser included

--- src/tlang/BasicParser.java
+++ src/tlang/BasicParser.java
@@ -1,18 +1,101 @@
 package tlang;
-import tlang.ast.*;
+
+import static tlang.Parser.*;
+
 import java.util.HashSet;
-import static tlang.Parser.rule;
+
 import tlang.Parser.Operators;
+import tlang.ast.ASTree;
+import tlang.ast.BinaryExpr;
+import tlang.ast.BlockStmnt;
+import tlang.ast.IfStmnt;
+import tlang.ast.Name;
+import tlang.ast.NegativeExpr;
+import tlang.ast.NumberLiteral;
+import tlang.ast.NullStmnt;
+import tlang.ast.PrimaryExpr;
+import tlang.ast.StringLiteral;
+import tlang.ast.WhileStmnt;
 
 public class BasicParser {
 	HashSet<String> reserved = new HashSet<String> ();
-	Operators opterators = new Operators();
+	Operators operators = new Operators();
 	Parser expr0 = rule();
 
 	Parser primary = rule(PrimaryExpr.class)
-					.or(rule().sep("(").ast(expr0).sep(""),
+					.or(rule().sep("(").ast(expr0).sep(")"),
 						rule().number(NumberLiteral.class),
 						rule().identifier(Name.class, reserved),
 						rule().string(StringLiteral.class));
-	Parser factor = rule().sep("-").ast(primary).or
+	/**primary : either a primary expression class, or
+	*			 Primary "(" expr ")" | number | identifier | string
+	*/
+	
+	Parser factor = rule().or(rule(NegativeExpr.class).sep("-").ast(primary), primary);
+	/**factor: a negative primary expression ( when it is a number ), or a primary expression
+	 *
+	 */
+	
+	Parser expr = expr0.expression(BinaryExpr.class, factor, operators);
+	
+	Parser statement0 =  rule();
+	
+	/**
+	 * block    : "{" [ statement ] {(";" | EOL) [ statement]} "}"
+	 */
+	Parser block = rule(BlockStmnt.class)
+			.sep("{").option(statement0)
+			.repeat(rule().sep(";", Token.EOL).option(statement0))
+			.sep("}");
+	
+	/**
+	 * simple   : expr
+	 */
+	Parser simple = rule(PrimaryExpr.class).ast(expr);
+	
+	/**
+	 * statement : "if" expr block [ "else" block ]
+	 *           | "while" expr block
+	 *           | simple  
+	 */
+	Parser statement = statement0.or(
+			rule(IfStmnt.class).sep("if").ast(expr).ast(block)
+						.option(rule().sep("else").ast(block)),
+			rule(WhileStmnt.class).sep("while").ast(expr).ast(block),
+			simple);
+	
+	/**
+	 * program   : [ statement ] (";" | EOL)
+	 */
+	Parser program = rule().or(statement, rule(NullStmnt.class))
+						.sep(";", Token.EOL);
+	
+	/**
+	 * 
+	 */
+	public BasicParser() {
+		reserved.add(";");
+		reserved.add("}");
+		reserved.add(Token.EOL);
+		
+		operators.add("=", 1, Operators.RIGHT);
+		operators.add("==", 2, Operators.LEFT);
+		operators.add(">", 2, Operators.LEFT);
+		operators.add("<", 2, Operators.LEFT);
+		operators.add("+", 3, Operators.LEFT);
+		operators.add("-", 3, Operators.LEFT);
+		operators.add("*", 4, Operators.LEFT);
+		operators.add("/", 4, Operators.LEFT);
+		operators.add("%", 4, Operators.LEFT);
+	}
+	
+	/**
+	 * 
+	 * @param lexer
+	 * @return
+	 * @throws ParseException
+	 */
+	public ASTree parse(Lexer lexer) throws ParseException {
+		return program.parse(lexer);
+	}
 }
